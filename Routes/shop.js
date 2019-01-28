87d3d1c76272bad611de2b8207977ee857bf9260
A Nagaraj (IT/Hosur/TVSMotor) <raj.naga51@gmail.com>
2019-01-28 19:43:51 +0530

    First commit

--- Routes/shop.js
+++ Routes/shop.js
@@ -1,11 +1,13 @@
 const express=require("express");
 
+const path=require("path");
+
 const router=express.Router();
 
 router.get("/",(req,res,next)=>{
     // console.log("This is last Middleware");
     // res.send("<h1>This is Final Middleware</h1>");
-    res.sendFile("./Views/shop.html");
+    res.sendFile(path.join(__dirname,"../","Views","shop.html"));
 });
 
 module.exports=router;
